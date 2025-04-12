6af014b79ec101cb6855ff0469d2c31c225cc836
Evan Si <evsi@amazon.com>
2025-04-12 01:18:11 +0000

    Update PortalDefineQuery call for postgres 18 compatibility
    525392d from postgres upstream adds a new argument in PortalDefineQuery,
    CachedPlanSource. The CachedPlan in this call is NULL so naturally
    CachedPlanSource is passed NULL here.

--- src/pg_cron.c
+++ src/pg_cron.c
@@ -2265,7 +2265,11 @@ ExecuteSqlString(const char *sql)
 		portal = CreatePortal("", true, true);
 		/* Don't display the portal in pg_cursors */
 		portal->visible = false;
-		PortalDefineQuery(portal, NULL, sql, commandTag, plantree_list, NULL);
+		#if PG_VERSION_NUM < 180000
+			PortalDefineQuery(portal, NULL, sql, commandTag, plantree_list, NULL);
+		#else
+			PortalDefineQuery(portal, NULL, sql, commandTag, plantree_list, NULL, NULL);
+		#endif
 		PortalStart(portal, NULL, 0, InvalidSnapshot);
 		PortalSetResultFormat(portal, 1, &format);		/* binary format */
 
